fe2f1f99bd60760389e125336f66aef239209953
chaerlo127 <monynony0203@gmail.com>
2022-08-02 14:00:13 +0900

    FEAT: API

--- introduction-spring/src/main/java/com/example/introductionspring/controller/HelloController.java
+++ introduction-spring/src/main/java/com/example/introductionspring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -22,6 +23,38 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name;
+    }
+
+
+    /*
+    * ResponseBody가 있으면 viewResolver에 넘겨주지 않고 바로 HttpMessageConverter에 객체를 넘겨준다.
+    * 문자가 넘어오면 viewResolver에서 StringHttpMessageConverter
+    * 객체가 넘어오면 viewResolver에서 MappingJackon2HttpMessageConverter -> json style로 변경됨
+    * 객체를 넘겨주면 json 형식으로 넘겨주는 것이 default
+    * */
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloAPI(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
     }
 
 }
