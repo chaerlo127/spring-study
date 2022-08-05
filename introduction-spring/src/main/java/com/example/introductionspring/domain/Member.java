1f3db3675e1aa27c91f19d30a666e17cad41f751
chaerlo127 <monynony0203@gmail.com>
2022-08-05 13:28:02 +0900

    FEAT: Create Member Domain and Repository

---
+++ introduction-spring/src/main/java/com/example/introductionspring/domain/Member.java
@@ -0,0 +1,13 @@
+package com.example.introductionspring.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+    private Long id;
+    private String name;
+
+}
---
+++ introduction-spring/src/main/java/com/example/introductionspring/repository/MemoryMemberRepository.java
@@ -0,0 +1,40 @@
+package com.example.introductionspring.repository;
+
+import com.example.introductionspring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    /*
+    * key 값을 생성해주는 것
+    * */
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+       return store.values().stream()
+                .filter(member -> member.getName().equals(name)) // 하나씩 이름을 비교하여
+                .findAny(); // 하나라도 찾으면 return
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+        // store.values() -> Map 내부에 저장된 Member 객체 반환
+    }
+}
