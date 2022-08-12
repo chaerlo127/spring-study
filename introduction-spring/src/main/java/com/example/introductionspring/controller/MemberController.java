0045f11d0b5c2be91338b6fec748ebf93a1ce7f6
chaerlo127 <monynony0203@gmail.com>
2022-08-12 21:58:25 +0900

    FEAT: member form summit api

--- introduction-spring/src/main/java/com/example/introductionspring/controller/MemberController.java
+++ introduction-spring/src/main/java/com/example/introductionspring/controller/MemberController.java
@@ -1,8 +1,13 @@
 package com.example.introductionspring.controller;
 
+import com.example.introductionspring.domain.Member;
 import com.example.introductionspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 /**
  * 객체를 생성해서 spring 컨테이너가 관리를 해줌
  * 스프링 빈 등록 방법
@@ -23,5 +28,19 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
---
+++ introduction-spring/src/main/java/com/example/introductionspring/controller/MemberForm.java
@@ -0,0 +1,11 @@
+package com.example.introductionspring.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class MemberForm {
+
+    @Getter
+    @Setter
+    private String name;
+}
